068bf9119e528b0b4d1ee6584180e25515495524
unknown <myfulllegalname@protonmail.com>
2021-07-07 11:23:10 -0400

    updated

--- plugin.js
+++ plugin.js
@@ -1,17 +1,20 @@
 import { BigNumber, utils } from 'https://cdn.skypack.dev/ethers';
+
+const DF_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/darkforest-eth/dark-forest-v06-round-2';
+const MARKET_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/zk-farts/dfartifactmarket';
+
 const SALES_CONTRACT_ADDRESS = "0x3Fb840EbD1fFdD592228f7d23e9CA8D55F72F2F8";
 const SALES_CONTRACT_ABI = await fetch('https://gist.githubusercontent.com/zk-FARTs/5761e33760932affcbc3b13dd28f6925/raw/afd3c6d8eba7c27148afc9092bfe411d061d58a3/MARKET_ABI.json').then(res=>res.json());
 const SALES = await df.loadContract(SALES_CONTRACT_ADDRESS,SALES_CONTRACT_ABI);
-const DF_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/darkforest-eth/dark-forest-v06-round-2';
-const MARKET_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/zk-farts/dfartifactmarket';
+
 const TOKENS_CONTRACT_ADDRESS = "0xafb1A0C81c848Ad530766aD4BE2fdddC833e1e96"; // when a new round starts someone has to change this
 const TOKENS_APPROVAL_ABI = await fetch('https://gist.githubusercontent.com/zk-FARTs/d5d9f3fc450476b40fd12832298bb54c/raw/1cac7c4638ee5d766615afe4362e6ce80ed68067/APPROVAL_ABI.json').then(res=>res.json());
 const TOKENS = await df.loadContract(TOKENS_CONTRACT_ADDRESS,TOKENS_APPROVAL_ABI);  
-const FEE = "5000000000000000" 
+const FEE = await SALES.fee() 
 
 /*
   createElement function: this lets me make elements more easily
-    @params params: Object containing 5 entries :TODO maybe a Map is faster
+    @params params: Object containing at most 5 entries
         params.type: The kind of element to create (string)
         params.attributes: a Map containing all the attributes. key = attribute, value = value
         params.text: The text of the element
@@ -42,8 +45,9 @@ function createElement(params){
 
 
 // fetch subgraph data for token stats and prices
-// returns object containing all the stats for every token owned by the current player and the market contract with prices for listed ones
 async function subgraphData(){
+    
+  // gets from df subgraph
   const tokens  = await fetch(DF_GRAPH_URL,{
   method: 'POST',
   headers: {
@@ -80,7 +84,8 @@ async function subgraphData(){
     `
       })
   })
-  
+
+  // gets from shop subgraph
   const prices = await fetch(MARKET_GRAPH_URL,{
     method: 'POST',
     headers: {
@@ -98,6 +103,7 @@ async function subgraphData(){
         `
     })
   })
+  
   const tokensObject = await tokens.json()
   const pricesArray = (await prices.json()).data.prices
   
@@ -111,13 +117,13 @@ async function subgraphData(){
 
 // function for properly formatting the artifacts stats
 function formatMultiplier(mul){
-    if (mul ==100){
+    if (mul ===100){
       return `+0%`
     }        
-    else if (mul <100){
-      return  `-${100 -mul}%`            
+    else if (mul >100){
+      return `+${mul-100}%`            
     }
-      return `+${mul-100}%`
+      return  `-${100 -mul}%`
   }
 
 // Creates one row in the table of the users withdrawn artifacts
@@ -136,17 +142,13 @@ function myRow(artifact){
 
     const row = document.createElement("tr")
     row.appendChild(createElement({type:"td", text:`${artifact.rarity} ${artifact.artifactType}` }))
-    row.appendChild(createElement({type:"td"})).appendChild(
-      createElement({type:"div", text:`
-        ${formatMultiplier(artifact.energyCapMultiplier)}
-        ${formatMultiplier(artifact.energyGrowthMultiplier)}
-        ${formatMultiplier(artifact.rangeMultiplier)}
-        ${formatMultiplier(artifact.speedMultiplier)}
-        ${formatMultiplier(artifact.defenseMultiplier)}`,
-        css: new Map([["fontSize","65%"]])})
-      )
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.energyCapMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.energyGrowthMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.rangeMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.speedMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.defenseMultiplier)}))
     row.appendChild(createElement({type:"td" })).appendChild(document.createElement("div")).appendChild(
-      createElement({type:"input", eventListeners:new Map([["change",onChange]]), css:new Map([["border","1px solid white"],["backgroundColor","#080808"]]), attributes:(new Map([["type","number"],["min",0],["step",0.01],["size",4]]))})
+      createElement({type:"input", eventListeners:new Map([["change",onChange]]), attributes:(new Map([["type","number"],["min",0],["step",0.01],["size",4]]))})
       ).parentNode.appendChild(
       createElement({type:"button", eventListeners:new Map([["click",onClick]]), text:"List"}))
     
@@ -165,20 +167,14 @@ function saleRow(artifact){
     }
     
     const row = document.createElement('tr')
-    row.appendChild(
-      createElement({type:"td", text:artifactNameFromArtifact(artifact.id) })).appendChild(
-        (createElement({type:"div", text:`${artifact.rarity} ${artifact.artifactType}`,}))).parentNode.appendChild(
-      createElement({type:"div", text:`
-        ${formatMultiplier(artifact.energyCapMultiplier)}
-        ${formatMultiplier(artifact.energyGrowthMultiplier)}
-        ${formatMultiplier(artifact.rangeMultiplier)}
-        ${formatMultiplier(artifact.speedMultiplier)}
-        ${formatMultiplier(artifact.defenseMultiplier)}`,
-        css: new Map([["fontSize","65%"]])})
-      )
-    
+    row.appendChild(createElement({type:"td", text:`${artifact.rarity} ${artifact.artifactType}` }))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.energyCapMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.energyGrowthMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.rangeMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.speedMultiplier)}))
+    row.appendChild(createElement({type:"td", text:formatMultiplier(artifact.defenseMultiplier)}))
     row.appendChild(createElement({type:"td"})).appendChild(
-      createElement({type:"div", text:`price: ${utils.formatEther(artifact.price)} XDAI + ${utils.formatEther(FEE)} fee`})
+      createElement({type:"div", text:`price: ${utils.formatEther(artifact.price)} XDAI + ${utils.formatEther(FEE)} fee`, css:new Map([["fontSize","70%"]])})
     ).parentNode.appendChild(
       createElement({type:"button",text:"Buy", eventListeners:new Map([["click",onClick]]) })
     )
@@ -190,14 +186,18 @@ function saleRow(artifact){
 // Creates the table of the users withdrawn artifacts
 function myTable(data){
 
-    const table = createElement({type:"table", css:new Map([["tableLayout","fixed"],["width","100%"]])})
+    const table= document.createElement('table')
     table.appendChild(
       createElement({type:"caption",text:"My Artifacts"}))
     table.appendChild(
       document.createElement('thead')).appendChild(
         document.createElement('tr')).appendChild(
           createElement({type:"th", text:"Artifact"})).parentNode.appendChild(
-          createElement({type:"th", text:"Stats"})).parentNode.appendChild(
+          createElement({type:"th", text:"Cap"})).parentNode.appendChild(
+          createElement({type:"th", text:"Growth"})).parentNode.appendChild(
+          createElement({type:"th", text:"Range"})).parentNode.appendChild(
+          createElement({type:"th", text:"Speed"})).parentNode.appendChild(
+          createElement({type:"th", text:"Defense"})).parentNode.appendChild(
           createElement({type:"th", text:"List"}))
     if (data !== null){
       const body = table.appendChild(document.createElement('tbody'))
@@ -212,14 +212,18 @@ function myTable(data){
 // Creates the table of the stores listed artifacts
 function saleTable(data){
   
-  const table= createElement({type:"table", css:new Map([["tableLayout","fixed"],["width","100%"]])})
+  const table= document.createElement('table')
   table.appendChild(
       createElement({type:"caption",text:"Store Artifacts"}))
   table.appendChild(
       document.createElement('thead')).appendChild(
         document.createElement('tr')).appendChild(
           createElement({type:"th", text:"Artifact"})).parentNode.appendChild(
-          createElement({type:"th", text:"Stats"})).parentNode.appendChild(
+          createElement({type:"th", text:"Cap"})).parentNode.appendChild(
+          createElement({type:"th", text:"Growth"})).parentNode.appendChild(
+          createElement({type:"th", text:"Range"})).parentNode.appendChild(
+          createElement({type:"th", text:"Speed"})).parentNode.appendChild(
+          createElement({type:"th", text:"Defense"})).parentNode.appendChild(
           createElement({type:"th", text:"Buy"}))
   if (data !== null){
     const body = table.appendChild(document.createElement('tbody'))
@@ -230,37 +234,55 @@ function saleTable(data){
   return table
 }
 
-// special buttons for approving the contract and if I can figure out refreshing it goes here too
-// I assume that re-rendering the whole thing would be async 
-async function specialButtons(){
+// special buttons for approving the contract and refreshing refreshing it goes here too
+
+async function specialButtons(container, plugin){
+  
   const approve = ()=> {
     TOKENS.setApprovalForAll(SALES_CONTRACT_ADDRESS,true).catch(e=>console.log(e)) // this will approve the market for all tokens
   }
-
+  const refresh = async ()=> {
+    plugin.destroy()
+    await plugin.render(container)
+    
+  }
   const div = document.createElement('div')
-  div.appendChild(createElement({type:"button", text:"approve", css:new Map([["float","right"]]), eventListeners:new Map([["click",approve]])}))
+  div.appendChild(createElement({type:"button", text:"approve", eventListeners:new Map([["click",approve]])}))
+  div.appendChild(createElement({type:"button", text:"refresh", eventListeners:new Map([["click",refresh]])}))  
   return div
 }
+
+function styleSheet(){
+    const style = document.createElement('style')
+    style.innerHTML=`
+        table { table-layout: fixed; width: 100% } 
+        input { border: 1px solid white; background-color: #080808}
+        th, td { text-align:center }
+        `
+    return style
+}
+
 
 class Plugin {
   constructor() {
     this.container=null;
-    this.TOKENS = null
   }
 
   async render(container) {
     const data=await subgraphData()
     this.container=container;
-    this.container.style.width="400px"
-    this.container.style.height="400px"
-    this.container.appendChild(await specialButtons())
+    this.container.style.width="500px"
+    this.container.appendChild(await specialButtons(container,this))
+    this.container.appendChild(styleSheet())
     this.container.appendChild(myTable(data))
     this.container.appendChild(saleTable(data))
 
   }
 
-  destroy() {
-    this.container = null;
+  destroy() {    
+    while (this.container.firstChild){
+        this.container.removeChild(this.container.firstChild)
+    }
   }
 }
 
